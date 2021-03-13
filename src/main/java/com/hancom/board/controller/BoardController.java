5446f86d83f858f06b1beb7f113c51986008de4d
dongmin <dongminmin94@gmail.com>
2021-03-14 05:31:07 +0900

    api 통신 중 PUT 메소드로 요청이 올 경우 변경하면 안되는 정보와 변경해도 되는 정보들을 구별하여 데이터베이스에 저장하는 로직을 구현

--- src/main/java/com/hancom/board/controller/BoardController.java
+++ src/main/java/com/hancom/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.hancom.board.controller;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -31,12 +32,15 @@ public class BoardController {
 	public List<BoardOutputDTO> get(){
 		
 		List<Board> boardList = boardService.get();
-		List<BoardOutputDTO> boardOutputDTOList;
+		List<BoardOutputDTO> boardOutputDTOList = new ArrayList<BoardOutputDTO>();
+		
+		//데이터베이스에 저장되어있는 모든 레코드를 boardList에 가져온 뒤 그 리스트의 각 요소들의 본문내용과 최신 수정일 필드를 제외하고 새로운 리스트를 만들어 반환해준다. 
 		boardList.forEach(e -> {
-			
+			boardOutputDTOList.add(BoardOutputDTO.builder().number(e.getNumber()).title(e.getTitle()).
+					author(e.getAuthor()).createdDate(e.getCreatedDate()).build());
 		});
 		
-		return boardService.get();
+		return boardOutputDTOList;
 	}
 	
 	//상세보기
@@ -44,6 +48,7 @@ public class BoardController {
 	public Board get(@PathVariable int number) {
 		
 		Board board = boardService.get(number);
+		
 		if(board == null) {
 			throw new RuntimeException(number + "번 게시물을 찾지 못했습니다.");
 		}
@@ -67,12 +72,29 @@ public class BoardController {
 	}
 	
 	//수정
+	//수정시에는 RequestBody에 Board 모델 클래스를 받아 수정하고자하는 게시물의 번호(기본키)를 알아내야 한다. 
+	//그러나 이러한 경우에는 유저가 임의로 변경하면 안되는 정보(번호, 최초 작성일)를 변경하여 보낼 수도 있다.
+	//따라서 이러한 문제를 해결하기 위해 해당 메소드에서 먼저 게시물의 번호가 동일한 객체를 가져온 뒤 변경할 정보들만 변경해주는 작업을 한 뒤 데이터베이스에 저장해주는 방식으로 구현하였다. 
 	@PutMapping("/board")
 	public Board update(@RequestBody Board board) {
 		
-		boardService.save(board);
+		Board originBoard = boardService.get(board.getNumber());
 		
-		return board;
+		if(originBoard == null) {
+			throw new RuntimeException(board.getNumber() + "번 게시물을 찾지 못했습니다.");
+		}
+		
+		//해당 포맷에 맞춰 최근 수정일을 저장한다.
+		SimpleDateFormat lastModifiedDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		
+		//RequestBody로부터 넘어온 Board 객체의 필드 정보들 중 변경이 허용된 필드들만 변경해준다.
+		originBoard.setTitle(board.getTitle());
+		originBoard.setAuthor(board.getAuthor());
+		originBoard.setContent(board.getContent());
+		originBoard.setLastModifiedDate(lastModifiedDate.format(new Date()).toString());
+		boardService.save(originBoard);
+		
+		return originBoard;
 	}
 	
 	//삭제
